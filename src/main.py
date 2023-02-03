75972bff0938f6dd77b68ef298be279cb787f763
ChristianLin0420 <crlc112358@gmail.com>
2023-02-03 23:44:03 +0800

    finished shell script

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ from run import run
 SETTINGS['CAPTURE_MODE'] = "fd" # set to "no" if you want to see stdout/stderr in console
 logger = get_logger()
 
-ex = Experiment("pymarl")
+ex = Experiment("alpha")
 ex.logger = logger
 ex.captured_out_filter = apply_backspaces_and_linefeeds
 
@@ -35,6 +35,32 @@ def my_main(_run, _config, _log):
     run(_run, config, _log)
 
 
+def open_yaml_file(path, config_name = None):
+    with open(path, "r") as f:
+        try:
+            config_dict = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            assert False, "{}.yaml error: {}".format(config_name, exc)
+    return config_dict
+
+
+def _get_map_config(params):
+    map_name = ""
+
+    for _i, _v in enumerate(params):
+        if _v.split("=")[0] == "--map_name":
+            map_name = _v.split("=")[1]
+            break
+
+    map_train_t = open_yaml_file(os.path.join(os.path.dirname(__file__), "config", "map.yaml"), "map_config")
+
+    map_config = {
+        "map_name": map_name,
+        "t_max": map_train_t[map_name] // 50000
+    }
+
+    return map_config
+
 def _get_config(params, arg_name, subfolder):
     config_name = None
     for _i, _v in enumerate(params):
@@ -44,12 +70,7 @@ def _get_config(params, arg_name, subfolder):
             break
 
     if config_name is not None:
-        with open(os.path.join(os.path.dirname(__file__), "config", subfolder, "{}.yaml".format(config_name)), "r") as f:
-            try:
-                config_dict = yaml.load(f)
-            except yaml.YAMLError as exc:
-                assert False, "{}.yaml error: {}".format(config_name, exc)
-        return config_dict
+        return open_yaml_file(os.path.join(os.path.dirname(__file__), "config", subfolder, "{}.yaml".format(config_name)), config_name)
 
 
 def recursive_dict_update(d, u):
@@ -76,24 +97,27 @@ if __name__ == '__main__':
     # Get the defaults from default.yaml
     with open(os.path.join(os.path.dirname(__file__), "config", "default.yaml"), "r") as f:
         try:
-            config_dict = yaml.load(f)
+            config_dict = yaml.safe_load(f)
         except yaml.YAMLError as exc:
             assert False, "default.yaml error: {}".format(exc)
 
     # Load algorithm and env base configs
     env_config = _get_config(params, "--env-config", "envs")
     alg_config = _get_config(params, "--config", "algs")
-    # config_dict = {**config_dict, **env_config, **alg_config}
+    map_config = _get_map_config(params)
+
+    env_config["env_args"]["map_name"] = map_config["map_name"]
     config_dict = recursive_dict_update(config_dict, env_config)
     config_dict = recursive_dict_update(config_dict, alg_config)
+    config_dict = recursive_dict_update(config_dict, map_config)
 
     # now add all the config to sacred
     ex.add_config(config_dict)
 
     # Save to disk by default for sacred
     logger.info("Saving to FileStorageObserver in results/sacred.")
-    file_obs_path = os.path.join(results_path, "sacred")
+    file_obs_path = os.path.join(results_path, "experiment{}".format(config_dict["experiment"]), "sacred", config_dict["map_name"], config_dict["mixer"])
     ex.observers.append(FileStorageObserver.create(file_obs_path))
 
-    ex.run_commandline(params)
-
+    # ex.run_commandline(params)
+    ex.run()
